70fd7ffd3ff3aa9d54e8446a44671ba26eba94bb
Alejandro Sanmiguel <alejandrosanmiguel0222@gmail.com>
2025-11-09 19:02:00 -0500

    Agregar configuración de ProGuard, permisos de red y archivo de configuración de seguridad de red

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,9 +32,10 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Usar las llaves de debug para simplicidad
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
